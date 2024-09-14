ee6ae61fc6f7a0e3661f77646b40200a84cd03e5
Sarthak <sarthak753@gmail.com>
2024-09-14 16:02:33 +0530

    Recyclable and Low Fat Products

---
+++ Leetcode 50/Find Customer Referee.sql
@@ -0,0 +1,24 @@
+-- https://leetcode.com/problems/find-customer-referee/
+
+-- Example 1:
+-- Input: 
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
